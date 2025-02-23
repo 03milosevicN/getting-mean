61534623a3590b4787d709f6ce439ddbda47dc96
03milosevicN <nikolamilosevic220803@gmail.com>
2025-02-23 18:42:10 +0100

    Added .dockerignore

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('about');
 });
 
 module.exports = router;
