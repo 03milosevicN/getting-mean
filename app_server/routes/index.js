3daaee23c78c7768661a4290d283b02e6b12e9d4
03milosevicN <nikolamilosevic220803@gmail.com>
2025-02-26 09:59:16 +0100

    Separated controllers.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-const mainController = require('../controllers/main');
+const mainController = require('../controllers/locations.controller');
 
 router.get('/', mainController.indexController);
 
